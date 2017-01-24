a5c5fb1fb5afa0363b415ffd7999ecf518d2c8b8
doyeonii <dy9502@gmail.com>
2017-01-24 21:38:57 +0900

    commit comment

---
+++ Boggle/src/Boggle.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Boggle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int total = sc.nextInt();
+		// int input = sc.nextInt(); //int로 받기
+		// double input = sc.nextDouble(); // double로 받기
+		// String input = sc.next(); //공백을 기준으로 문자열 받기
+		int stunum;// student number
+		int pair;
+		for (int i = 0; i < total; i++) {
+			stunum = sc.nextInt();
+			pair = sc.nextInt();
+			for (int j = 0; j < pair; j++) {
+				
+			}
+
+		}
+
+	}
+
+}
